1d2a5f0a2964ffceb05f573147c939b56b94a82f
simony-nouran <nouranessam1111@gmail.com>
2023-06-08 17:44:29 -0700

    w

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+from sys import argv
 
-import sys
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    arglen = len(argv) - 1
+    if arglen == 0:
+        print('0 arguments.')
+    elif arglen == 1:
+        print('1 argument:')
+        print('1: {}'.format(argv[1]))
+    else:
+        print('{} arguments:'.format(arglen))
+        for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
